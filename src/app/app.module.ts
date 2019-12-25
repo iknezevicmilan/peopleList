a965ef7e520bcac97d51574fd963b512c20da784
Milan Knežević <milan.knezevic@asseco-see.rs>
2019-12-25 15:07:21 +0100

    Bljuc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PeopleListComponent } from './people-list/people-list.component';
 import { PeopleService } from './people.service';
+import { DepartmentListComponent } from './department-list/department-list.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PeopleListComponent
+    PeopleListComponent,
+    DepartmentListComponent,
+    EmployeeListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
